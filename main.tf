1751929c6e9dd7e44fb7350d12740835cc0cbce8
Luke McCleary <luke.mccleary@gmail.com>
2022-11-28 13:03:00 -0500

    bug

--- main.tf
+++ main.tf
@@ -2,7 +2,10 @@ provider "aws" {
   default_tags {
    tags = local.tags
 }
-region = var.region
+  region = var.region
+  access_key = data.doormat_aws_credentials.creds.access_key
+  secret_key = data.doormat_aws_credentials.creds.secret_key
+  token      = data.doormat_aws_credentials.creds.token
 }
 
 provider "doormat" {}
@@ -11,12 +14,6 @@ data "doormat_aws_credentials" "creds" {
   provider = doormat
 
   role_arn = "arn:aws:iam::325038557378:role/sample_dev-custom_role"
-}
-
-provider "aws" {
-  access_key = data.doormat_aws_credentials.creds.access_key
-  secret_key = data.doormat_aws_credentials.creds.secret_key
-  token      = data.doormat_aws_credentials.creds.token
 }
 
 locals {
